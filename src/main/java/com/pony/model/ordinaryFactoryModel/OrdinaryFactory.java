bfc75ee010e2d5b64c6134c72f6e8fc70ab6a57b
heiPony <1083365468@qq.com>
2019-11-10 14:14:51 +0800

    ------------

--- src/main/java/com/pony/model/ordinaryFactoryModel/OrdinaryFactory.java
+++ src/main/java/com/pony/model/ordinaryFactoryModel/OrdinaryFactory.java
@@ -1,25 +1,32 @@
 package com.pony.model.ordinaryFactoryModel;
 
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
 //简单工厂模式(静态工厂方法模式)定义一个类创建其他类，被创建的实例通常都有共同的父类
 public class OrdinaryFactory {
         public static FruitStore get(String name){
-
             try {
-                Class fruit = Class.forName(name);
+                Properties pro = new Properties();
+                ClassLoader classLoader = OrdinaryFactory.class.getClassLoader();
+                InputStream is = classLoader.getResourceAsStream("pro.properties");
+                pro.load(is);
+                String className = pro.getProperty("className");
+                Class fruit = Class.forName(className+"."+name);
+                Method getFruit = fruit.getMethod("getFruit");
                 return  (FruitStore) fruit.newInstance();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
             return null;
         }
 
     public static void main(String[] args) {
-        FruitStore fruitStore = OrdinaryFactory.get("com.pony.model.ordinaryFactoryModel.Apple");
-        fruitStore.getFruit();
+        FruitStore apple = OrdinaryFactory.get("Apple");
+        FruitStore banana = OrdinaryFactory.get("Banana");
+        apple.getFruit();
+        banana.getFruit();
     }
 }
 
